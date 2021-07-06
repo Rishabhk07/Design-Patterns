981a607725236076f13884ac74149c6958e3206c
Rishabhk07 <rishabhkhanna777@gmail.com>
2021-07-05 19:25:15 -0700

    singleton

---
+++ src/chapter_5_singleton/Singleton.java
@@ -0,0 +1,27 @@
+package chapter_5_singleton;
+
+public class Singleton {
+    // Another method to singleton thread safe os that we initialize it on the class initialization.
+    // private  volatile static Singleton uniqueInstance = new Singleton();
+
+    //    Another way to make this singleton thread safe is to make it volatile,
+    //    and then synchronising the new Singleton creation code
+    //    which makes only the new creation of the singleton synchronised
+    private  volatile static Singleton uniqueInstance;
+
+//   private static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized (Singleton.class) {
+                if(uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
