51de23845f48a8aee3511b22947999f83e3af5c9
Rishabhk07 <rishabhkhanna777@gmail.com>
2021-10-26 23:00:23 -0700

    iterator

---
+++ src/chapter9_iterator_composite/DinerMenuIterator.java
@@ -0,0 +1,22 @@
+package chapter9_iterator_composite;
+
+public class DinerMenuIterator implements Iterator {
+    MenuItem[] items;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < items.length && items[position] != null;
+    }
+
+    @Override
+    public Object next() {
+        MenuItem menuItem = items[position];
+        position++;
+        return menuItem;
+    }
+}
---
+++ src/chapter9_iterator_composite/PancakeHouseIterator.java
@@ -0,0 +1,25 @@
+package chapter9_iterator_composite;
+
+import java.util.ArrayList;
+
+public class PancakeHouseIterator implements  Iterator{
+
+    ArrayList<MenuItem> menuItems;
+    int position;
+
+    public PancakeHouseIterator(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < menuItems.size() && menuItems.get(position) != null;
+    }
+
+    @Override
+    public Object next() {
+        MenuItem menuItem = menuItems.get(position);
+        position++;
+        return menuItem;
+    }
+}
